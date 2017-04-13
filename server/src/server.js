27492b7fec5ae6e31190d5a6c12ac32cec2872cc
Harsh Poddar <me@harsh.io>
2017-04-13 13:28:20 -0400

    static

--- server/src/server.js
+++ server/src/server.js
@@ -6,27 +6,11 @@ var Util = require("./util");
 var app = express();
 app.use(bodyParser.text());
 
-// Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// You run the server from `server`, so `../client/build` is `server/../client/build`.
+// '..' means "go up one directory", so this translates into `client/build`!
+app.use(express.static('../client/build'));
 
 // Starts the server on port 3000!
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-});
-
-// Handle POST /reverse [data]
-app.post('/reverse', function (req, res) {
-  var reverseString = Util.reverseString;
-  // If the request came with text, then the text() middleware handled it
-  // and made `req.body` a string.
-  // Check that req.body is a string.
-  if (typeof(req.body) === 'string') {
-    var reversed = reverseString(req.body);
-    res.send(reversed);
-  } else {
-    // POST did not contain a string. Send an error code back!
-    res.status(400).end()
-  }
 });
